d9a75652d49cfcf163dde6b0bb6c187b05adb801
Sérgio <sergio.filho@pdcase.com.br>
2026-07-14 13:55:10 -0300

    refatoracao de codigo

--- src/main/java/miranda/sergio/comercio/repository/LojaRepository.java
+++ src/main/java/miranda/sergio/comercio/repository/LojaRepository.java
@@ -2,19 +2,15 @@ package miranda.sergio.comercio.repository;
 
 import miranda.sergio.comercio.dto.Loja;
 import miranda.sergio.comercio.enums.Categoria;
-import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
-import java.awt.print.Pageable;
 import java.util.List;
 
 public interface LojaRepository extends JpaRepository<Loja, Long> {
 
     @Query("""
-    SELECT DISTINCT l
-    FROM Loja l
+    SELECT DISTINCT l FROM Loja l
     LEFT JOIN l.tags t
     WHERE
     LOWER(l.nome) LIKE LOWER(CONCAT('%', :texto, '%'))
@@ -23,21 +19,16 @@ public interface LojaRepository extends JpaRepository<Loja, Long> {
     """)
     List<Loja> pesquisar(@Param("texto") String texto);
 
-
     @Query("""
-    SELECT DISTINCT l
-    FROM Loja l
+    SELECT DISTINCT l FROM Loja l
     LEFT JOIN l.tags t
     WHERE
-    (
-        LOWER(l.nome) LIKE LOWER(CONCAT('%', :texto, '%'))
-        OR LOWER(t.nome) LIKE LOWER(CONCAT('%', :texto, '%'))
+    ( LOWER(l.nome) LIKE LOWER(CONCAT('%', :texto, '%'))
+      OR LOWER(t.nome) LIKE LOWER(CONCAT('%', :texto, '%'))
     )
     AND l.categoria = :categoria
     """)
-    List<Loja> pesquisarPorTextoECategoria(
-            @Param("texto") String texto,
-            @Param("categoria") Categoria categoria);
+    List<Loja> pesquisarPorTextoECategoria(@Param("texto") String texto, @Param("categoria") Categoria categoria);
 
     List<Loja> findByCategoria(Categoria categoria);
 
