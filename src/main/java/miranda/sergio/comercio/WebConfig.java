4fae8428859777b00b03c4afe13f0e39d3a1942a
Sérgio <sergio.filho@pdcase.com.br>
2026-07-27 14:53:37 -0300

    ajuste cors

--- src/main/java/miranda/sergio/comercio/WebConfig.java
+++ src/main/java/miranda/sergio/comercio/WebConfig.java
@@ -14,6 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins(
                         "http://localhost:4200",
                         "http://localhost:3000",
+                        "https://verocentro.com",
                         "https://verocentro.com.br",
                         "https://www.verocentro.com.br"
                 )
